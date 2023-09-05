23e8fd4cf01d6752fed94f7d2872cb9e74d7adbc
EntireHall <kk23223511@gmail.com>
2023-09-05 21:03:06 +0530

    last commit

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -6,27 +6,27 @@ function Resume() {
       <div className=' text-normal text-green-700 underline underline-offset-4'>
         <h1 className=' font-bold font-pop text-5xl mb-6 mt-20 max-sm:pl-7'>Education</h1>
 
-        <div className='pl-[20%] mt-6'>
-          <h1 className='text-3xl font-bold mb-6 '>Matriculation:-</h1>
-          <p className='text-xl text-slate-300 pl-40'>Government model senior secondary school —Sector-19C Chandigarh —
+        <div className='md:pl-[20%] mt-6'>
+          <h1 className='text-3xl font-bold mb-6 max-sm:pl-10'>Matriculation:-</h1>
+          <p className='text-xl text-slate-300 md:pl-40 max-sm:pl-20'>Government model senior secondary school —Sector-19C Chandigarh —
             Matriculation (10TH) with 62%.</p>
         </div>
 
-        <div className='pl-[20%] mt-6'>
-          <h1 className='text-3xl font-bold mb-6 '>Intermediate:-</h1>
-          <p className='text-xl text-slate-300 pl-40'>Government model senior secondary school —Sector-23A Chandigarh —
+        <div className='md:pl-[20%] mt-6'>
+          <h1 className='text-3xl font-bold mb-6 max-sm:pl-10'>Intermediate:-</h1>
+          <p className='text-xl text-slate-300 md:pl-40 max-sm:pl-20 '>Government model senior secondary school —Sector-23A Chandigarh —
             Intermidiate (12TH) with 77%.
           </p>
         </div>
 
-        <div className='pl-[20%] mt-6'>
-          <h1 className='text-3xl font-bold mb-6 '>Graduation:-</h1>
-          <p className='text-xl text-slate-300 pl-40'>post graduate government college —Sector-11c Chandigarh —Graduate (BCA)
+        <div className='md:pl-[20%] mt-6'>
+          <h1 className='text-3xl font-bold mb-6 max-sm:pl-10 '>Graduation:-</h1>
+          <p className='text-xl text-slate-300 md:pl-40 max-sm:pl-20'>post graduate government college —Sector-11c Chandigarh —Graduate (BCA)
             with 78%.</p>
         </div>
-        <div className='pl-[20%] mt-6'>
-          <h1 className='text-3xl font-bold mb-6 '>Certfificate:-</h1>
-          <p className='text-xl text-slate-300 pl-40'>Got 79 percentage in Computer Application 1-Year Course. In which i learnt Photoshop, Coreldraw, MicrosoftOffice.</p>
+        <div className='md:pl-[20%] mt-6'>
+          <h1 className='text-3xl font-bold mb-6 max-sm:pl-10 '>Certfificate:-</h1>
+          <p className='text-xl text-slate-300 md:pl-40 max-sm:pl-20 '>Got 79 percentage in Computer Application 1-Year Course. In which i learnt Photoshop, Coreldraw, MicrosoftOffice.</p>
         </div>
 
       </div>
