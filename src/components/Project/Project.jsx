bd92546c510b13d6ce46f98f9644754250044988
EntireHall <kk23223511@gmail.com>
2023-09-11 13:54:07 +0530

    fourth commit

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -5,7 +5,7 @@ function Rand({hell,div1})
 
 
 {
-  return<div className='text-slate-300 text-2xl font-pop text-center md:px-2 mb-2.5 '>
+  return<div className='text-slate-300 text-2xl font-pop flex flex-col justify-center items-center text-center px-2 mb-2.5 '>
 
       <h1 className='font-bold text-green-700 '>{hell}</h1>
       <span className='text-lg'>{div1}</span>
@@ -17,13 +17,25 @@ export default function Service() {
 
   return (
     
-     <div>
+     <div className='flex flex-col justify-center items-center flex-wrap gap-10 min-h-screen bg-zinc-950 mix-blend-screen'>
 
-<h1>Projects Made  </h1>
+<h1 className='md:text-6xl font-pop font-extrabold text-slate-200  mb-10 md:mt-40 max-sm:text-4xl max-sm:mt-16'>Projects Made  </h1>
     {
       Record.map((data,index) => (
-<div>
-  </div>
+
+<div className='border-4 border-green-700 md:max-w-[60%] max-sm:border-none flex-wrap md:p-6' key={index} >
+        
+       
+          <Rand hell="Project Name :" div1={data.name}/>
+          <Rand hell="Tools Used :" div1={data.tools}/>
+          <Rand hell="About :" div1={data.para}/>
+          <div>
+            <button title='Hello'>Code</button>
+          <Rand hell="Source Code :" div1={data.code}/>
+          <Rand hell="Live demo:" div1={data.demo}/>
+          </div>
+         </div>
+
      
   
   ))
