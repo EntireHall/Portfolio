2a0a367f526478241a1686a164ea9feddd31b14e
EntireHall <kk23223511@gmail.com>
2023-09-11 20:05:18 +0530

    form sorted

--- src/components/Contact/Form/Form.jsx
+++ src/components/Contact/Form/Form.jsx
@@ -1,41 +1,41 @@
 import React from 'react'
-import { useState } from 'react'
 
-const Form = () => {
+function Form () {
 
-    function Input({ type, placeholder, name}) {
-        return <input type={type} name={name} placeholder={placeholder}  className='bg-transparent rounded border-2 mb-2
+    function Input({ type, placeholder, name }) {
+        return <input type={type} name={name} placeholder={placeholder} required className='bg-transparent rounded border-2 mb-2
          border-red-600 pt-4 p-2 max-sm:pr-20  md:pr-40 outline-none  ' />
-        
+
     }
-    
-    function Input1({ type, placeholder, name}) {
-        return <input type={type} name={name} placeholder={placeholder}  className='bg-transparent rounded border-2 mb-2
+
+    function Input1({ type, placeholder, name }) {
+        return <input type={type} name={name} placeholder={placeholder} required className='bg-transparent rounded border-2 mb-2
          border-red-600 pt-4 p-2 md:pr-40 max-sm:pr-20  outline-none pb-40 max-sm:mr-4' />
-        
     }
+
+        
 
     return (
 
-        <form  action="https://getform.io/f/a51cda11-91ee-4d48-b33a-934cf848c2d1" method="POST" autoComplete='off' className=' border-2 border-green-700
+        <form action="https://getform.io/f/a51cda11-91ee-4d48-b33a-934cf848c2d1" method="POST" autoComplete='off' className=' border-2 border-green-700
          flex flex-col justify-center items-center flex-wrap md:p-10  mt-10 text-slate-200 mix-blend-screen' >
 
             <div className='flex flex-wrap justify-center items-center '>
 
                 <div className='grid gap-4 max-sm:mt-4'>
-                    <Input type="text" name="name"  placeholder='Your Name'  />
-                    <Input type="phone" name="phone"   placeholder='Your Phone' />
-                    <Input type="email" name="email"   placeholder='Your Email' />
+                    <Input type="text" name="name" placeholder='Your Name' />
+                    <Input type="phone" name="phone" placeholder='Your Phone' />
+                    <Input type="email" name="email" placeholder='Your Email' />
                 </div>
                 <div className='flex flex-wrap pl-4'>
-                    <Input1 type="text" name="message"  placeholder='Your Message'/>
+                    <Input1 type="text" name="message" placeholder='Your Message' />
                 </div>
             </div>
 
             <div className='flex justify-center'>
-            <button type="submit" className=' mt-10 w-40 rounded text-zinc-900 font-semibold bg-green-700 p-2  text-lg transition ease-in duration-300 delay-200
+                <button type="submit" className=' mt-10 w-40 rounded text-zinc-900 font-semibold bg-green-700 p-2  text-lg transition ease-in duration-300 delay-200
              hover:text-green-700 hover:bg-transparent hover:border-2 hover:border-green-700 mb-6  '>Send</button>
-</div>
+            </div>
         </form>
 
     )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import {CgMenuHotdog,CgClose} from "react-icons/cg"
 
 
 function Li({ link, title }) {
-    return <Link to={link} smooth duration={800} className="font-semibold text-green-800 hover:underline underline-offset-4 ">
-        <li className=''><li className='hover:text-white transition-all duration-200 ease-in-out hover:text-xl'>{title}</li></li>
+    return <Link to={link} className="font-semibold text-green-800 hover:underline underline-offset-4 max-sm:pr-40">
+        <li className=''><li className='hover:text-white transition-all duration-200 ease-in-out hover:text-xl '>{title}</li></li>
     </Link>
 }
 
@@ -15,11 +15,11 @@ export default function Navbar() {
     
     return (
         
-        <div className=" flex justify-around p-4 bg-zinc-950 shadow-md shadow-gray-500 font-pop "> 
+        <div className=" flex justify-around p-4 bg-zinc-950 shadow-md shadow-gray-500 font-pop  "> 
             
-               <h1 className=' font-bold text-slate-200 text-2xl  animate-pulse w-full px-4 font-Aer'>Portfoilio.</h1>
+               <h1 className=' font-bold text-slate-200 text-2xl animate-pulse w-full px-4 font-Aer'>Portfoilio.</h1>
                 
-                <ul className={` md:flex md:justify-end gap-10 md:w-full text-lg px-6  ${show ? "sm:flex  md:border-hidden border-b mt-14 px-20  mr-[20%]": "hidden "}` }>
+                <ul className={` md:flex md:justify-end gap-10 md:w-full text-lg px-6  ${show ? "sm:flex  md:border-hidden mt-14  ": "hidden "}` }>
                     <Li link={"/"} title={"Home"} />
                     <Li link={"/about"} title={"About"} />
                     <Li link={"/skill"} title={"Skill"} />
